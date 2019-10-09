0c24d72bcc1690923115bedeed71477617cb7064
Arthur Edelstein <arthuredelstein@gmail.com>
2019-10-08 17:29:04 -0700

    Log tracking events

--- background.js
+++ background.js
@@ -1,7 +1,23 @@
 // see https://searchfox.org/mozilla-central/rev/7088fc958db5935eba24b413b1f16d6ab7bd13ea/uriloader/base/nsIWebProgressListener.idl#259
-const STATE_COOKIES_BLOCKED_TRACKER = 0x20000000;
-
-loadedTabMap = new Map();
+const STATE = {
+  0x00001000: "BLOCKED_TRACKING_CONTENT",
+  0x00002000: "LOADED_TRACKING_CONTENT",
+  0x00000040: "BLOCKED_FINGERPRINTING_CONTENT",
+  0x00000400: "LOADED_FINGERPRINTING_CONTENT",
+  0x00000800: "BLOCKED_CRYPTOMINING_CONTENT",
+  0x00200000: "LOADED_CRYPTOMINING_CONTENT",
+  0x00004000: "BLOCKED_UNSAFE_CONTENT",
+  0x00008000: "COOKIES_LOADED",
+  0x00040000: "COOKIES_LOADED_TRACKER",
+  0x00080000: "COOKIES_LOADED_SOCIAL_TRACKER",
+  0x10000000: "COOKIES_BLOCKED_BY_PERMISSION",
+  0x20000000: "COOKIES_BLOCKED_TRACKER",
+  0x40000000: "COOKIES_BLOCKED_ALL",
+  0x80000000: "COOKIES_PARTITIONED_FOREIGN",
+  0x00000080: "COOKIES_BLOCKED_FOREIGN",
+  0x00010000: "BLOCKED_SOCIALTRACKING_CONTENT",
+  0x00020000: "LOADED_SOCIALTRACKING_CONTENT",
+};
 
 browser.contentBlocking.useBasicList();
 
@@ -11,7 +27,7 @@ let countBlockedTrackingCookies = function (log) {
   let blockedCount = 0;
   for (let [origin, actions] of Object.entries(JSON.parse(log))) {
     for (let [state, blocking] of actions) {
-      if (blocking && state === STATE_COOKIES_BLOCKED_TRACKER) {
+      if (blocking && STATE[state] === "COOKIES_BLOCKED_TRACKER") {
         blockedCount++;
       }
     }
@@ -19,52 +35,62 @@ let countBlockedTrackingCookies = function (log) {
   return blockedCount;
 };
 
+let blockingLogToCSV = function (time, tabAddress, log) {
+  let csvLines = [];
+  for (let [origin, actions] of Object.entries(JSON.parse(log))) {
+    for (let [state, blocking, num] of actions) {
+      if (blocking) {
+        let tag = STATE[state] || `undefined_${state}`;
+        let timeISO = time.toISOString();
+        csvLines.push(`${timeISO}\t${tabAddress}\t${origin}\t${tag}\n`);
+      }
+    }
+  }
+  return csvLines.join("");
+};
+
 var getTotalCount = async () => {
   let stored = await browser.storage.local.get("totalCount");
   return stored["totalCount"] || 0;
 };
 
-let accumulate = async (count) => {
+var getCSVLines = async () => {
+  let stored = await browser.storage.local.get("csv");
+  return stored["csv"] || "";
+};
+
+let accumulate = async ({count, csv}) => {
   let totalCount = await getTotalCount();
   totalCount += count;
   await browser.storage.local.set({"totalCount": totalCount});
+  let csvLines = await getCSVLines();
+  csvLines += csv;
+  await browser.storage.local.set({"csv": csvLines});
 };
 
 var resetCount = async () => {
   await browser.storage.local.set({"totalCount": 0});
+  await browser.storage.local.set({"csv": ""});
 };
 
-let countBlockedTrackingCookiesInTab = async tabId => {
+let trackingDataForTab = async tabId => {
+  let url = (await browser.tabs.get(tabId)).url
   let log = await browser.contentBlocking.getContentBlockingLog(tabId);
-  return countBlockedTrackingCookies(log);
+  return {
+    count: countBlockedTrackingCookies(log),
+    csv: blockingLogToCSV(new Date(), url, log)
+  };
 };
 
-let tallyCookiesBlockedInTab = async tabId => {
-  let count = await countBlockedTrackingCookiesInTab(tabId);
-  await accumulate(count);
+let storeTrackingDataForTab = async tabId => {
+  let {count, csv} = await trackingDataForTab(tabId);
+  await accumulate({count, csv});
 };
-
-browser.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
-  let tabCookieCount = loadedTabMap.get(tabId);
-  if (tabCookieCount !== undefined) {
-    await accumulate(tabCookieCount);
-  }
-  loadedTabMap.delete(tabId);
-});
 
 let navigationEndedHandler = async details => {
   if (details.parentFrameId === -1) {
-    let count = await countBlockedTrackingCookiesInTab(details.tabId);
-    loadedTabMap.set(details.tabId, count);
+    await storeTrackingDataForTab(details.tabId);
   }
 };
 
 browser.webNavigation.onCompleted.addListener(navigationEndedHandler);
-browser.webNavigation.onDOMContentLoaded.addListener(navigationEndedHandler);
-browser.webNavigation.onErrorOccurred.addListener(navigationEndedHandler);
-
-browser.webNavigation.onBeforeNavigate.addListener(async details => {
-  if (details.parentFrameId === -1) {
-    await tallyCookiesBlockedInTab(details.tabId);
-  }
-});
--- popup.js
+++ popup.js
@@ -2,13 +2,28 @@ let update = async totalCount => {
   document.getElementById("test").innerHTML = "Total cookies blocked: " + totalCount;
 };
 
+// from https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
 (async () => {
-  let backgroundWindow = await browser.runtime.getBackgroundPage();
-  let totalCount = await backgroundWindow.getTotalCount();
+  let backgroundPage = await browser.runtime.getBackgroundPage();
+  let totalCount = await backgroundPage.getTotalCount();
   await update(totalCount);
   document.getElementById("resetButton").addEventListener("click", async () => {
-    await backgroundWindow.resetCount();
+    await backgroundPage.resetCount();
     await update(0);
+  });
+  document.getElementById("downloadLog").addEventListener("click", async () => {
+    let log = await backgroundPage.getCSVLines();
+    download("trackingLog.txt", log);
   });
 })();
 
